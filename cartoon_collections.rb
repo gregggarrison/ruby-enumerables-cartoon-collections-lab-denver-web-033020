0d6be864555a74612082620f0ef88dfc021c69ea
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-22 16:21:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+ array.each_with_index {|item, index| puts "#{index + 1} #{item}"}
 end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
