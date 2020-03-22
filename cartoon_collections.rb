00835fc50b136131ce895afc49f54e7a55a212ed
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-22 17:01:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+ array.each_with_index {|item, index| puts "#{index + 1} #{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+array.collect do |names|
+    names.capitalize << "!"
+  end
+end 
+
+def long_planeteer_calls(array)
+array.any? { |call| call.length > 4 }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)#
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |ingredient|
+    cheese_types.include?(ingredient)
+  end
 end
